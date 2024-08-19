5a66db44035c9f8aef519c93709417d7d577aba9
BZSH <99548050@qq.com>
2024-08-20 07:39:17 +0800

    init pinia

--- src/main.js
+++ src/main.js
@@ -6,8 +6,11 @@ import 'element-plus/dist/index.css'
 
 import router from './router'
 
-const app = createApp(App)
+import pinia from './store'
 
+
+const app = createApp(App)
+app.use(pinia);
 app.use(ElementPlus)
 app.use(router) //注册路由
 app.mount('#app')
